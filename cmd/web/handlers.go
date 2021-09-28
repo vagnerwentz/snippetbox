51390353a0722f30f73bb8a70dce42561380c13a
Vagner Wentz <wentz.vagner@gmail.com>
2021-09-27 22:57:04 -0300

    feat: improve the home function to render the templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"log"
 	"fmt"
-	"net/http"
 	"strconv"
+	"net/http"
+	"html/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +14,36 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello from SnippetBox"))
+	// Initialize a slice containing the paths to the two files. Note that the
+	// home.page.tpl file must be the *first* file in the slice.
+	files := []string{
+		"./ui/html/home.page.tpl",
+		"./ui/html/base.layout.tpl",
+		"./ui/html/footer.partial.tpl",
+	}
+
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and use
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we can pass the slice of file paths
+	// as a variadic parameter?
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// We then use the Execute() method on the template set to write the template
+	// content as the response body. The last parameter to Execute() represents any
+	// dynamic data that we want to pass in, which for now we'll leave as nil.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
